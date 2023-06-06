da3e233268b6781f9b8804c6d40826c9588b4cb8
Mico02 <mohamed.hussein122702@gmail.com>
2023-06-06 18:44:30 -0400

    Updated UserJoinedeEvent to welcome user on a dedicated welcome channel

--- src/main/java/events/UserJoinedEvent.java
+++ src/main/java/events/UserJoinedEvent.java
@@ -1,5 +1,6 @@
 package events;
 
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -7,8 +8,26 @@ public class UserJoinedEvent extends ListenerAdapter {
 
 	@Override
 	public void onGuildMemberJoin(GuildMemberJoinEvent e) {
-		String name = e.getUser().getAsMention();
-		e.getGuild().getTextChannels().get(0).sendMessage("Welcome to the server! " + name).queue();
+		String newMemberName = e.getUser().getAsMention(); // Getting the new member name in for @NAME
+		String serverName = e.getGuild().getName(); // Getting the server name
+
+		TextChannel welcomeChannel = null; // Intializing a TextChannelß
+		;
+		try {
+			// Sets welcomeChannel to the existing #welcome channel on the server
+			welcomeChannel = e.getGuild().getTextChannelsByName("welcome", true).get(0);
+		} catch (IndexOutOfBoundsException exp) {
+			/*
+			 * If #welcome channel does not exist, then #welcome channel is created and
+			 * welcomeChannel is set accordingly
+			 */
+			e.getGuild().createTextChannel("welcome").complete();
+			System.out.println("CAUGHT EXCEPTION");
+			welcomeChannel = e.getGuild().getTextChannelsByName("welcome", true).get(0);
+		}
+
+		// Sending welcome message to user
+		welcomeChannel.sendMessage("Welcome to " + serverName + "! " + newMemberName).queue();
 	}
 
 }
