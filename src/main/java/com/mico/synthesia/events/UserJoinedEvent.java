e992151726067efa007e8802d53698658bddd5df
Mico02 <mohamed.hussein122702@gmail.com>
2023-06-06 23:58:04 -0400

    Added a feature where the #welcome channel is put in a welcome category

--- src/main/java/com/mico/synthesia/events/UserJoinedEvent.java
+++ src/main/java/com/mico/synthesia/events/UserJoinedEvent.java
@@ -1,5 +1,7 @@
 package com.mico.synthesia.events;
 
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.channel.concrete.Category;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -10,21 +12,26 @@ public class UserJoinedEvent extends ListenerAdapter {
 	public void onGuildMemberJoin(GuildMemberJoinEvent e) {
 		String newMemberName = e.getUser().getAsMention(); // Getting the new member name in for @NAME
 		String serverName = e.getGuild().getName(); // Getting the server name
-
-		TextChannel welcomeChannel = null; // Intializing a TextChannel
+		Guild guild = e.getGuild();
+		TextChannel welcomeChannel = null; // Initializing a TextChannel
 		try {
 			// Sets welcomeChannel to the existing #welcome channel on the server
-			welcomeChannel = e.getGuild().getTextChannelsByName("welcome", true).get(0);
+			welcomeChannel = guild.getTextChannelsByName("welcome", true).get(0);
 		} catch (IndexOutOfBoundsException exp) {
 			/*
-			 * If #welcome channel does not exist, then #welcome channel is created and
-			 * welcomeChannel is set accordingly
+			 * If #welcome channel does not exist, then #welcome channel is created and put
+			 * in a welcome category. If welcome category does not exist, then one is
+			 * created.
 			 */
-			e.getGuild().createTextChannel("welcome").complete();
-			welcomeChannel = e.getGuild().getTextChannelsByName("welcome", true).get(0);
+			Category welcomeCategory = (guild.getCategoriesByName("welcome", true).size() < 1)
+					? guild.createCategory("Welcome").complete()
+					: guild.getCategoriesByName("welcome", true).get(0);
+			guild.modifyCategoryPositions().selectPosition(welcomeCategory).moveTo(0).queue();
+			welcomeChannel = guild.createTextChannel("welcome", welcomeCategory).complete();
+			guild.modifyTextChannelPositions().selectPosition(welcomeChannel).moveTo(0).queue();
 		}
 
-		// Sending welcome message to user
+		// Sending welcome message to user in the #welcome channel
 		welcomeChannel.sendMessage("Welcome to " + serverName + "! " + newMemberName).queue();
 	}
 
